a7ec6108082948d59c69f60001b91109b7f7c0b3
Jamie Pinkham <jamie@mobelux.com>
2011-05-05 11:38:48 -0400

    changed __mycompanyname__

--- Event.m
+++ Event.m
@@ -3,7 +3,7 @@
 //  CoreDataFinders
 //
 //  Created by Jamie Pinkham on 5/5/11.
-//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//  Copyright 2011 Jamie Pinkham. All rights reserved.
 //
 
 #import "Event.h"
